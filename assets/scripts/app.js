dcc5c51b7b7cb0777a25fde60adc2bc7a73ccda0
ako-21 <akeemogle@gmail.com>
2020-06-29 17:34:17 -0400

    Sign In and Sign Up complete Styling for Sign in and Sign up complete along with event listeners to show and hide sign in and sign up forms

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,10 +2,17 @@
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
-
+const events = require('./events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  $('#signUp').on('submit', events.onSignUp)
+  $('#signIn').on('submit', events.onSignIn)
+  $('#changePWD').on('submit', events.onChangePWD)
+
+
+
+  $('#tosignup').on('click', () => { $('.sign-in-hide').hide(); $('.sign-up-hide').show() })
+  $('#tosignin').on('click', () => { $('.sign-up-hide').hide(); $('.sign-in-hide').show() })
 })
